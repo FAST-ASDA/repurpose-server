f941d5a894c08fccc6c8693e0ab5cc821d1f96e3
Shiva Gupta <shivagupta4639@gmail.com>
2021-11-06 00:32:02 +0530

    Update makeOrder Controller

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,48 +1,47 @@
 const axios = require("axios");
 const asyncHandler = require("express-async-handler");
 const db = require("../config/db.js");
-const { uploadS3 } = require("../utils/uploadaws.js")
-const { dummyOrders } = require('../helper/dummyOrders.js')
+const { uploadS3 } = require("../utils/uploadaws.js");
+const { dummyOrders } = require("../helper/dummyOrders.js");
 
 const insertDummyOrders = asyncHandler(async (req, res, next) => {
-    let values = dummyOrders(1000)
-    console.log(values)
-    db.query("INSERT INTO orders (products, userId,sellerId, status,deliveryCharge,basePrice,ifPaid) VALUES ?", [values], async (err, results) => {
-        if (err) {
-            console.log(err)
-            res.status(500)
-            next(new Error("SERVER ERROR"))
-        }
-        else {
-            res.status(200).json({
-                msg: "Orders created",
-                success: true,
-            });
-        }
-    })
-
-})
-
+  let values = dummyOrders(1000);
+  console.log(values);
+  db.query(
+    "INSERT INTO orders (products, userId,sellerId, status,deliveryCharge,basePrice,ifPaid) VALUES ?",
+    [values],
+    async (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        next(new Error("SERVER ERROR"));
+      } else {
+        res.status(200).json({
+          msg: "Orders created",
+          success: true,
+        });
+      }
+    }
+  );
+});
 
 const getAllOrderDetails = asyncHandler(async (req, res, next) => {
-    db.query(`SELECT * FROM orders`, async (err, results) => {
-        if (err) {
-            console.log(err)
-            res.status(500)
-            next(new Error("SERVER ERROR"))
-        }
-        else {
-            res.status(200).json({
-                msg: "Order Details",
-                data: {
-                    Orders: results
-                },
-                success: true,
-            });
-         
-        }
-    })
-})
+  db.query(`SELECT * FROM orders`, async (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500);
+      next(new Error("SERVER ERROR"));
+    } else {
+      res.status(200).json({
+        msg: "Order Details",
+        data: {
+          Orders: results,
+        },
+        success: true,
+      });
+    }
+  });
+});
 
 // async function getPriceFromProduct(productId) {
 
@@ -78,111 +77,137 @@ const getAllOrderDetails = asyncHandler(async (req, res, next) => {
 //     return originalBasePrice
 // }
 async function getBasePrice(products) {
-    let productDetails=[];
-    // console.log(products)
-    const promises = products.map((product) => fetchProductDetails(product, productDetails));
-    await Promise.all(promises);
-    return productDetails
+  let productDetails = [];
+  // console.log(products)
+  const promises = products.map((product) =>
+    fetchProductDetails(product, productDetails)
+  );
+  await Promise.all(promises);
+  return productDetails;
 }
-  
-const fetchProductDetails=(product, productDetails)=> {
 
-    return new Promise((resolve, reject) => {
-        db.query(`SELECT * from products WHERE productId=?`,[product],
-            async (err, results) => {
-                if (err) {
-                    console.log(err)
-                    reject(err);
-                }
-                else {
-                    resolve(productDetails.push(results[0]))
-                }
-            }
-        )
-    })
-}
-const makeOrder = asyncHandler(async (req, res, next) => {
-    const { products } = req.body;
-
-   const productDetails = await getBasePrice(products)
-
-    if(productDetails){
-        console.log(productDetails)
-    }
-    // db.query(`INSERT INTO orders (products, userId,sellerId, status,deliveryCharge,basePrice) VALUES (?)`,
-    //     [[JSON.stringify(products), userId, sellerId, status, deliveryCharge, basePrice]],
-    //     async (err, results) => {
-    //         if (err) {
-    //             console.log(err)
-    //             res.status(500)
-    //             next(new Error("SERVER ERROR"))
-    //         }
-    //         else {
-    //             res.status(200).json({
-    //                 msg: "Order Created",
-    //                 data: {
-    //                     orderId: results.insertId
-    //                 },
-    //                 success: true,
-    //             });
-    //         }
-    //     }
-    // )
-})
-const updatePaymentDetails = asyncHandler(async (req, res, next) => {
-    const { orderId, metadata } = req.body;
-    db.query(`UPDATE orders SET ifPaid=1`,
-        async (err, results) => {
-            if (err) {
-                console.log(err)
-                res.status(500)
-                next(new Error("SERVER ERROR"))
-            }
-        }
-    )
-    db.query(`INSERT INTO transactions (orderId,metadata) VALUES (?)`, [[orderId, JSON.stringify(metadata)]],
-        async (err, results) => {
-            if (err) {
-                console.log(err)
-                res.status(500)
-                next(new Error("SERVER ERROR"))
-            }
-            else {
-                res.status(200).json({
-                    msg: "Transaction Successful",
-                    data: {
-                        transactionId: results.insertId
-                    },
-                    success: true,
-                });
-            }
-        }
-    )
-})
-const getOrderDetails = asyncHandler(async (req, res, next) => {
-    const { orderId } = req.query;
-    db.query(`SELECT * FROM orders WHERE orderId=?`, orderId, async (err, results) => {
+const fetchProductDetails = (product, productDetails) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT * from products WHERE productId=?`,
+      [product],
+      async (err, results) => {
         if (err) {
-            console.log(err)
-            res.status(500)
-            next(new Error("SERVER ERROR"))
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(productDetails.push(results[0]));
         }
-        else {
-            res.status(200).json({
-                msg: "Order Details",
-                data: {
-                    Order: results[0]
-                },
-                success: true,
-            });
-        }
-    })
-})
+      }
+    );
+  });
+};
+const makeOrder = asyncHandler(async (req, res, next) => {
+  const { products, sellerId, basePrice } = req.body;
+
+  let originalPrice = 0;
+
+  const productDetails = await getBasePrice(products);
+
+  for (let product of productDetails) {
+    console.log(product.basePrice);
+    if (product === null) {
+      originalPrice = basePrice;
+      break;
+    } else {
+      originalPrice += product.basePrice;
+    }
+  }
+  if (typeof originalPrice !== "number") {
+    originalPrice = basePrice;
+  }
+  const commission = parseFloat(originalPrice) * 0.02;
+  const deliveryCharge = 10;
+  db.query(
+    `INSERT INTO orders (products, userId,sellerId,deliveryCharge,basePrice,commission) VALUES (?)`,
+    [
+      [
+        JSON.stringify(products),
+        req.user.userId,
+        sellerId,
+        deliveryCharge,
+        originalPrice,
+        commission,
+      ],
+    ],
+    async (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        next(new Error("SERVER ERROR"));
+      } else {
+        res.status(200).json({
+          msg: "Order Created",
+          data: {
+            orderId: results.insertId,
+          },
+          success: true,
+        });
+      }
+    }
+  );
+});
+const updatePaymentDetails = asyncHandler(async (req, res, next) => {
+  const { orderId, metadata } = req.body;
+  db.query(`UPDATE orders SET ifPaid=1`, async (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500);
+      next(new Error("SERVER ERROR"));
+    }
+  });
+  db.query(
+    `INSERT INTO transactions (orderId,metadata) VALUES (?)`,
+    [[orderId, JSON.stringify(metadata)]],
+    async (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        next(new Error("SERVER ERROR"));
+      } else {
+        res.status(200).json({
+          msg: "Transaction Successful",
+          data: {
+            transactionId: results.insertId,
+          },
+          success: true,
+        });
+      }
+    }
+  );
+});
+const getOrderDetails = asyncHandler(async (req, res, next) => {
+  const { orderId } = req.query;
+  db.query(
+    `SELECT * FROM orders WHERE orderId=?`,
+    orderId,
+    async (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        next(new Error("SERVER ERROR"));
+      } else {
+        res.status(200).json({
+          msg: "Order Details",
+          data: {
+            Order: results[0],
+          },
+          success: true,
+        });
+      }
+    }
+  );
+});
 
 module.exports = {
-    getAllOrderDetails,
-    insertDummyOrders,
-    makeOrder,
-    getOrderDetails,
-    updatePaymentDetails
-}
+  getAllOrderDetails,
+  insertDummyOrders,
+  makeOrder,
+  getOrderDetails,
+  updatePaymentDetails,
+};
